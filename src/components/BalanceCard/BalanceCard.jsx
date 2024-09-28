8933ead7491bc0f549059b7cc9be6507d27a9e0b
Ezeozue Chiagoziem <ezeozuechiagoziem@gmail.com>
2024-09-28 19:19:53 +0100

    Fix front end

--- src/components/BalanceCard/BalanceCard.jsx
+++ src/components/BalanceCard/BalanceCard.jsx
@@ -5,7 +5,7 @@ const BalanceCard = ({ balance }) => {
         <div className="bg-white shadow-lg rounded-lg p-6 w-full max-w-lg mt-8 border">
           <div className="flex justify-between items-center mb-4">
             <h2 className="text-lg font-semibold">Your Balance</h2>
-            <span className="text-sm text-gray-500">Ethereum</span>
+            <span className="text-sm text-gray-500"></span>
           </div>
 
           <div className="flex items-center space-x-2 mb-6">
