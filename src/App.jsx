2c19dd10cb369239201b041ee387f67c1c689d08
Ezeozue Chiagoziem <ezeozuechiagoziem@gmail.com>
2024-09-28 18:16:39 +0100

    Clear React Error

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import React, { useState, useEffect } from "react";
 import walletImage from "./assets/wallet-address.png";
 import BalanceCard from "./components/balanceCard/balanceCard";
--- src/components/BalanceCard/BalanceCard.jsx
+++ src/components/BalanceCard/BalanceCard.jsx
@@ -1,5 +1,4 @@
-import React from 'react'
-
+/* eslint-disable react/prop-types */
 const BalanceCard = ({ balance }) => {
   return (
     <>
